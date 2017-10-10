dd348375032bc4d78f36f5d14c1c62e5153ce355
Kotola Mikko M <mkotola@da3-cs-dk110-13.pc.helsinki.fi>
2017-10-10 14:15:11 +0300

    Lisätty testidataa ja laskureiden muokkaus testidatan mukaan.

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -4,11 +4,15 @@
 INSERT INTO Marjastaja (id, kayttajatunnus, salasana, etunimi, sukunimi) VALUES (1, 'kalle1', 'ahven', 'Kalle', 'Aaltonen');
 INSERT INTO Marjastaja (id, kayttajatunnus, salasana, etunimi, sukunimi) VALUES (2, 'jukka1', 'lahna', 'Jukka', 'Tatti');
 INSERT INTO Marjastaja (id, kayttajatunnus, salasana, etunimi, sukunimi) VALUES (3, 'mikko1', 'hauki', 'Mikko', 'Marjastaja');
+ALTER SEQUENCE Marjastaja_id_seq RESTART WITH 4;
 
 -- Marja-taulun testidata --
 INSERT INTO Marja (id, nimi) VALUES (1, 'Puolukka');
 INSERT INTO Marja (id, nimi) VALUES (2, 'Mustikka');
 INSERT INTO Marja (id, nimi) VALUES (3, 'Kanttarelli');
+INSERT INTO Marja (id, nimi) VALUES (4, 'Suppilovahvero');
+INSERT INTO Marja (id, nimi) VALUES (5, 'Herkkutatti');
+ALTER SEQUENCE Marja_id_seq RESTART WITH 6;
 
 -- Paikka-taulun testidata --
 INSERT INTO Paikka (id, marjastaja_id, p, i, nimi) VALUES (1, 1, 61.707724, 25.705254, 'Itäjärvi');
@@ -22,6 +26,7 @@ INSERT INTO Paikka (id, marjastaja_id, p, i, nimi) VALUES (8, 1, 61.507724, 25.5
 INSERT INTO Paikka (id, marjastaja_id, p, i, nimi) VALUES (9, 1, 61.607724, 25.605254, 'Soralampi');
 INSERT INTO Paikka (id, marjastaja_id, p, i, nimi) VALUES (10, 1, 62.307724, 25.705254, 'Mustikkapaikka');
 INSERT INTO Paikka (id, marjastaja_id, p, i, nimi) VALUES (11, 1, 62.407724, 25.805254, 'Tienpää');
+ALTER SEQUENCE Paikka_id_seq RESTART WITH 12;
 
 -- Kaynti-taulun testidata --
 INSERT INTO Kaynti (id, paikka_id, aika) VALUES (1, 1, NOW());
@@ -35,6 +40,8 @@ INSERT INTO Kaynti (id, paikka_id, aika) VALUES (8, 4, NOW());
 INSERT INTO Kaynti (id, paikka_id, aika) VALUES (9, 4, NOW());
 INSERT INTO Kaynti (id, paikka_id, aika) VALUES (10, 4, NOW());
 INSERT INTO Kaynti (id, paikka_id, aika) VALUES (11, 4, NOW());
+INSERT INTO Kaynti (id, paikka_id, aika) VALUES (12, 7, NOW());
+ALTER SEQUENCE Kaynti_id_seq RESTART WITH 13;
 
 -- Suosikkimarja-taulun testidata --
 INSERT INTO Suosikkimarja (marjastaja_id, marja_id) VALUES (1, 1);
@@ -55,3 +62,12 @@ INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (1, 3, 1.4, 
 INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (1, 8, 3.3, 'Jatkoin siitä mihin edellisellä käynnillä jäin.');
 INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (2, 8, 4.0, 'Erittäin hyviä marjoja!');
 INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (1, 9, 0.0, 'En poiminut ollenkaan, olivat raakoja.');
+INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (4, 3, 10.4, 'Paljon sieniä!');
+INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (5, 3, 5.0, 'Todella kiinteitä olivat.');
+INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (1, 8, 15.0, 'Runsas saalis.');
+INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (1, 8, 1.4, 'Olivat aika märkiä.');
+INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (1, 8, 2.4, 'Nyt pakastimeen.');
+INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (1, 12, 5.4, 'Taas oli hyvin.');
+INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (2, 12, 6.4, 'Hyvää marjaa!');
+INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (4, 12, 3.3, 'Nälkä meinasi tulla kun poimi.');
+INSERT INTO Marjasaalis (marja_id, kaynti_id, maara, kuvaus) VALUES (5, 12, 1.1, 'Hyvä saalis.');
