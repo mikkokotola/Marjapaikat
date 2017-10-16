780254a9f39fede92420237a6d1f0d71690ac281
Kotola Mikko M <mkotola@da3-cs-dk110-13.pc.helsinki.fi>
2017-10-16 12:58:43 +0300

    Toteutettu Kaynti-malli.

--- app/models/Kaynti.php
+++ app/models/Kaynti.php
@@ -6,20 +6,21 @@
  *
  * @author mkotola
  */
-
 class Kaynti {
+
     public $id, $paikka_id, $aika;
-    
+
     public function __construct($attributes) {
         parent::__construct($attributes);
+        $this->validators = array('validoi_aika');
     }
-    
+
     public static function all() {
         $query = DB::connection()->prepare('SELECT * FROM Kaynti');
         $query->execute();
         $rows = $query->fetchAll();
         $kaynnit = array();
-        
+
         foreach ($rows as $row) {
             $kaynnit[] = new Kaynti(array(
                 'id' => $row['id'],
@@ -29,22 +30,75 @@ class Kaynti {
         }
         return $kaynnit;
     }
-    
-    public static function find($id){
+
+    public static function find($id) {
         $query = DB::connection()->prepare('SELECT * FROM Kaynti WHERE id=:id LIMIT 1');
-        $query->execute(array('id'=> $id));
+        $query->execute(array('id' => $id));
         $row = $query->fetch();
-        
-        if ($row){
+
+        if ($row) {
             $kaynti = new Kaynti(array(
                 'id' => $row['id'],
                 'paikka_id' => $row['paikka_id'],
                 'aika' => $row['aika']
             ));
-        
+
             return $kaynti;
         }
         return null;
+    }
+
+    public static function haePaikanMukaan($paikka_id) {
+        $query = DB::connection()->prepare('SELECT * FROM Kaynti WHERE paikka_id=:paikka_id');
+        $query->execute(array('paikka_id' => $paikka_id));
+        $rows = $query->fetchAll();
+        $kaynnit = array();
+
+        foreach ($rows as $row) {
+            $kaynnit[] = new Kaynti(array(
+                'id' => $row['id'],
+                'paikka_id' => $row['paikka_id'],
+                'aika' => $row['aika']
+            ));
+        }
+        return $kaynnit;
+    }
+
+    public function tallenna() {
+        $query = DB::connection()->prepare('INSERT INTO Kaynti (paikka_id, aika) VALUES (:paikka_id, :aika) RETURNING id');
+        $query->execute(array(
+            'paikka_id' => $this->paikka_id,
+            'aika' => $this->aika
+        ));
+        $row = $query->fetch();
+        $this->id = $row['id'];
+    }
+
+    public function tallennaMuuttunut() {
+        $query = DB::connection()->prepare('UPDATE Kaynti SET aika = :aika WHERE id = :id;');
+        $query->execute(array(
+            'id' => $this->id,
+            'aika' => $this->aika
+        ));
+    }
+
+    public function poista() {
+        $query = DB::connection()->prepare('DELETE FROM Kaynti WHERE id = :id');
+        $query->execute(array('id' => $this->id));
+    }
+
+    public function validoi_aika() {
+        $errors = array();
+        
+        // Tarkastetaan, että on timestamp.
+        $format = 'Y-m-d H:i:s';
+        $d = DateTime::createFromFormat($format, $this->aika);
+        
+        if (!($d && $d->format($format) == $this->aika)) {
+            $errors[] = "Käynnin aika ei ole validi. Syötä muodossa YYYY-MM-DD HH:MM:SS.SSSS";
+        }
+        
+        return $errors;
     }
 
 }
